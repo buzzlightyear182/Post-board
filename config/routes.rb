f34aa84476c0a8fcd9fd2748d1dc5aa631c1963e
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-12-01 18:06:26 +0800

    Added github link to view

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#welcome', via: :get
+  root 'dashboards#show', via: :get
 
   # get '/dashboard' => 'dashboard#show'
 
